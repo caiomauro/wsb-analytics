119de3c54125e32ef2d044f21741c828979cf082
Caio Mauro <caiomauro18@gmail.com>
2024-03-18 02:08:31 -0400

    path fix

--- server/server/settings.py
+++ server/server/settings.py
@@ -56,7 +56,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://192.168.1.107:3000',
     'http://192.168.1.177',
-    'https://wsb-analytics.vercel.app/'
+    'https://wsb-analytics.vercel.app'
 ]
 
 ROOT_URLCONF = "server.urls"
