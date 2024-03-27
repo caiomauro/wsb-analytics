9cdf9e03195d8d98598bdd4e91c19b7d92c77bc2
Caio Mauro <101437705+caiomauro@users.noreply.github.com>
2024-03-27 11:35:26 -0400

    visual loader added

--- client/src/Pages/AnalyticsPage.tsx
+++ client/src/Pages/AnalyticsPage.tsx
@@ -3,7 +3,6 @@ import BottomHeader from "../Components/BottomHeader";
 import Navbar from "../Components/Navbar";
 import { fetchData, fetchDataRange, fetchLimitStockMentions } from "../Services/ServerApi";
 import getPreviousWeekStartDate from "../Utils/PreviousWeekStrUtil";
-import visualizationPieChart from "../Utils/VisualizationPieUtil";
 import visualizationTimeline from "../Utils/VisualizationTimelineUtil";
 import visualization from "../Utils/VisualizationUtil";
 
@@ -21,9 +20,14 @@ function AnalyticsPage() {
   const [isTooLarge, setIsTooLarge] = useState(false);
   const [stock, setStock] = useState("NVDA");
   const [allStocks, setAllStocks] = useState<string[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [loadingText, setLoadingText] = useState("Pulling data");
 
 
   useEffect(() => {
+    
+    setLoadingText("Establishing web-workers")
+
     const workerBar = new Worker(new URL('../Utils/VisualizationUtil', import.meta.url));
     const workerBarTimeline = new Worker(new URL('../Utils/VisualizationTimelineUtil', import.meta.url));
     const workerPie = new Worker(new URL('../Utils/VisualizationPieUtil', import.meta.url));
@@ -44,16 +48,22 @@ function AnalyticsPage() {
       setPieData(e.data);
     }
 
+    setLoadingText("Processing main data")
+
     fetchData(10000, 10).then(({ sortedStocks, stockSentiments, entries }) => {
       setAllStocks(sortedStocks);
       //setData(visualization(stockSentiments, entries));
       workerBar.postMessage({ data: stockSentiments, entries });
     });
 
+    setLoadingText("Processing pie chart data")
+
     fetchLimitStockMentions(10).then(( limitStockMentions ) => {
       //setPieData(visualizationPieChart(limitStockMentions.limitStockMentions.reverse()));
       workerPie.postMessage({data: limitStockMentions.limitStockMentions.reverse()});
     })
+
+    setLoadingText("Processing range data")
 
     fetchDataRange("NVDA",getPreviousWeekStartDate(), 1).then(({ stockSentiments, days, stock }) => {
       //setRangeData(visualizationTimeline(stock, stockSentiments, days));
@@ -66,6 +76,9 @@ function AnalyticsPage() {
     handleResize();
 
     window.addEventListener("resize", handleResize);
+
+    setLoadingText("Enjoy!")
+    setLoading(false);
 
     return () => {
       window.removeEventListener("resize", handleResize);
@@ -102,11 +115,22 @@ function AnalyticsPage() {
   return (
     <div className="flex flex-col w-full h-full custom-background-img-mobile sm:custom-background-img-desktop">
       <Navbar />
+      {loading ? (
+        <div className="flex flex-col h-screen w-full items-center gap-2 pt-40">
+        <div role="status">
+          <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          <span className="sr-only">Loading...</span>
+      </div>
+        <h1 className="font-bold text-2xl sm:text-4xl text-blue-500">{loadingText}</h1>
+        </div>
+        ) : (
       <div
         id="analytics-container"
         className="flex flex-col h-max w-full items-center overflow-y-auto gap-2 pt-8"
       >
-        <h1>Sentiment Analysis</h1>
         <div
           id="count-button-container"
           className="flex flex-row w-full sm:w-2/4 justify-around item-center pt-8"
@@ -219,6 +243,7 @@ function AnalyticsPage() {
           </Suspense>
         </div>
       </div>
+      )}
       <BottomHeader />
     </div>
   );
